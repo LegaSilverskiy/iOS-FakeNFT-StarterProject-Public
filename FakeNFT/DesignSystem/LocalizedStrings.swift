9fffc9843cf5d2827c57d8d2b6d7becafea02e55
Andrey Zhelev <anzhelev@gmail.com>
2024-08-11 23:30:04 +0300

    изменения в файлах Colors, Localizable, LocalizedStrings

--- FakeNFT/DesignSystem/LocalizedStrings.swift
+++ FakeNFT/DesignSystem/LocalizedStrings.swift
@@ -6,4 +6,9 @@ extension String {
     static let tabBarItemsCatalog = NSLocalizedString("tabBarController.tab.catalog", comment: "")
     static let tabBarItemsCart = NSLocalizedString("tabBarController.tab.cart", comment: "")
     static let tabBarItemsStatistics = NSLocalizedString("tabBarController.tab.statistics", comment: "")
+    static let sortingOptionsName = NSLocalizedString("sortingOptions.name", comment: "")
+    static let sortingOptionsRating = NSLocalizedString("sortingOptions.rating", comment: "")
+    static let buttonsCancel = NSLocalizedString("buttons.cancel", comment: "")
+    static let actionSheetTitleSorting = NSLocalizedString("actionSheet.title.sorting", comment: "")
+    
 }
