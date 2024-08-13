fb424b49efdc879dacf53f2a59b772689d70c5c6
Karina <barakhaeva@inbox.ru>
2024-08-13 16:45:29 +0300

    Создан файл CartPresenter

---
+++ FakeNFT/Scenes /Cart/Presenters/CartPresenter.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+final class CartViewPresenter {
+    let servicesAssembly: ServicesAssembly
+    
+    init(servicesAssembly: ServicesAssembly) {
+        self.servicesAssembly = servicesAssembly
+        
+    }
+}
