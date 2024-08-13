eeddab9031a226069f6c0849332ac50e79cd91be
LegaSilverskiy <slepchenko98@yandex.ru>
2024-08-13 22:41:26 +0500

    Создал начальные файлы для работы с каталогом

---
+++ FakeNFT/Scenes /Catalog/CatalogModel/CatalogMainScreenModel.swift
@@ -0,0 +1,13 @@
+//
+//  CatalogMainScreenModel.swift
+//  FakeNFT
+//
+//  Created by Олег Серебрянский on 8/13/24.
+//
+
+import Foundation
+import Combine
+
+final class CatalogMainScreenModel {
+    
+}
---
+++ FakeNFT/Scenes /Catalog/CustomCells/CustomCellTableView.swift
@@ -0,0 +1,8 @@
+//
+//  CustomCellTableView.swift
+//  FakeNFT
+//
+//  Created by Олег Серебрянский on 8/13/24.
+//
+
+import Foundation
