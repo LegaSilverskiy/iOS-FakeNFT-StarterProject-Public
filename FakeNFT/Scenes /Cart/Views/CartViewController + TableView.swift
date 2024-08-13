fc3d5883776f8af3e9fafef96fa83e1d56ebbb6e
Karina <barakhaeva@inbox.ru>
2024-08-13 17:39:05 +0300

    Добавлена таблица на экран корзины

---
+++ FakeNFT/Scenes /Cart/Views/CartViewController + TableView.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+extension CartViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        140
+    }
+}
--- FakeNFT/Scenes /Cart/Views/CartViewController.swift
+++ FakeNFT/Scenes /Cart/Views/CartViewController.swift
@@ -4,6 +4,13 @@ final class CartViewController: UIViewController {
     
     private let presenter: CartPresenter
     
+    private let nftsTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
     init(servicesAssembly: ServicesAssembly) {
         self.presenter = CartPresenter(servicesAssembly: servicesAssembly)
         super.init(nibName: nil, bundle: nil)
@@ -15,8 +22,45 @@ final class CartViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
         
+    }
+    
+    private func setupUI() {
         view.backgroundColor = .systemBackground
+        setupViews()
+        
+    }
+    
+    private func setupViews() {
+        setupTableView()
+        setupSortButton()
+    }
+    
+    private func setupSortButton() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "navBarItem.sort"), style: .plain, target: self, action: #selector(sortButtonPressed))
+        navigationItem.rightBarButtonItem?.tintColor = .tabBarItemsTintColor
+        navigationItem.rightBarButtonItem?.width = 44
+    }
+    
+    private func setupTableView() {
+        nftsTableView.delegate = self
+        nftsTableView.dataSource = self
+        
+        nftsTableView.separatorStyle = .none
+        nftsTableView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
+        
+        view.addSubview(nftsTableView)
+        NSLayoutConstraint.activate([
+            nftsTableView.topAnchor.constraint(equalTo: view.topAnchor),
+            nftsTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            nftsTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            nftsTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    @objc
+    private func sortButtonPressed() {
         
     }
 }
