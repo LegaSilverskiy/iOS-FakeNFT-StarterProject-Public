0ddfd4494f932b20a5d21988a3e081a797385456
LegaSilverskiy <slepchenko98@yandex.ru>
2024-08-21 22:03:33 +0500

    Test commit

--- FakeNFT/Scenes /Catalog/CatalogViewController.swift
+++ FakeNFT/Scenes /Catalog/CatalogViewController.swift
@@ -8,7 +8,7 @@ final class CatalogViewController: UIViewController {
         self.servicesAssembly = servicesAssembly
         super.init(nibName: nil, bundle: nil)
     }
-    
+    //mark:--
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
