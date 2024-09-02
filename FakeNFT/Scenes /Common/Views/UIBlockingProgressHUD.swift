b6088c191eda65b7dfa9e6016d5d8a2ba4f5603a
Karina <barakhaeva@inbox.ru>
2024-09-02 14:05:51 +0300

    Изменен значок индикатора загрузки и поправлен файл локализации

--- FakeNFT/Scenes /Common/Views/UIBlockingProgressHUD.swift
+++ FakeNFT/Scenes /Common/Views/UIBlockingProgressHUD.swift
@@ -16,7 +16,7 @@ final class UIBlockingProgressHUD {
 
     static func show() {
         window?.isUserInteractionEnabled = false
-        ProgressHUD.animationType = .activityIndicator
+        ProgressHUD.animationType = .ballVerticalBounce
         ProgressHUD.show()
     }
 
